d5db79958e15d916b897073fd215388a7383dafd
Abdulkadir Yaktı <abdulkadiryakti49@gmail.com>
2023-10-12 00:55:11 +0300

    first commit

---
+++ lib/components/bouncing_button.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class BouncingButton extends StatefulWidget {
+  final Widget child;
+  final VoidCallback onPress;
+
+  BouncingButton({
+    required this.child,
+    required this.onPress,
+  });
+
+  @override
+  _BouncingButtonState createState() => _BouncingButtonState();
+}
+
+class _BouncingButtonState extends State<BouncingButton>
+    with SingleTickerProviderStateMixin {
+  double _scale = 1.0;
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 150),
+      lowerBound: 0.0,
+      upperBound: 0.05,
+    );
+    _controller.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _scale = 1 - _controller.value;
+    return Listener(
+      onPointerDown: (PointerDownEvent event) {
+        _controller.forward();
+      },
+      onPointerUp: (PointerUpEvent event) {
+        _controller.reverse();
+        widget.onPress();
+      },
+      child: Transform.scale(
+        scale: _scale,
+        child: widget.child,
+      ),
+    );
+  }
+}
